e60abce3bed4ec9cefbaa11334f6901d1b8a5c67
berna39 <kalemajoseph39@gmail.com>
2022-09-08 10:57:50 +0200

    Caching data with redis

--- index.js
+++ index.js
@@ -10,22 +10,84 @@ const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const API_URL = "https://jsonplaceholder.typicode.com/albums/";
 
 const redisClient = redis.createClient(REDIS_PORT);
+redisClient.connect();
 
 app.get('/', (_, res) => { 
     res.send("Hello from API");
 });
 
 app.get('/albums', (_, res) => {
+    let startDate = new Date();
+    let endDate = new Date();
     try {
         axios.get(`${API_URL}`).then((response) => {
-
+            endDate = new Date();
             const albums = response.data;
-            console.log("Albums successfully retrieved from the API");
-            res.status(200).send(albums);
-
+            res.status(200).send({
+                data: albums,
+                time: endDate.getTime() - startDate.getTime()
+            });
         });
     } catch (error) {
         res.status(500).send({error: error.message});
+    }
+});
+
+app.get('/albums/:id', (req, res) => {
+    const id = req.params.id;
+    let startDate = new Date();
+    let endDate = new Date();
+    try {
+        axios.get(`${API_URL}?id=${id}`).then((response) => {
+            endDate = new Date();
+            const albums = response.data;
+            res.status(200).send({
+                data: albums,
+                time: endDate.getTime() - startDate.getTime()
+            });
+        });
+    } catch (error) {
+        res.status(500).send({error: error.message});
+    }
+});
+
+app.get('/albums-cached/:id', async (req, res) => {
+    const id = req.params.id;
+    let startDate = new Date();
+    let endDate = new Date();
+    try {
+
+        const album = await redisClient.get(id);
+        console.log(album);
+
+        if(album !== null)
+        {
+            endDate = new Date();
+            console.log("Album successfully retrieved from Redis");
+            res.status(200).send({
+                data: JSON.parse(album),
+                time: endDate.getTime() - startDate.getTime()
+            });
+        }
+        else
+        {
+            axios.get(`${API_URL}?id=${id}`).then((response) => {
+
+                const album = response.data;
+                redisClient.set(id, JSON.stringify(album));
+                endDate = new Date();
+
+                console.log("Album successfully retrieved from the API");
+        
+                res.status(200).send({
+                    data: album,
+                    time: endDate.getTime() - startDate.getTime()
+                });
+            });
+        }
+
+    } catch (error) {
+        res.status(500).send({ error: error.message });
     }
 });
 
